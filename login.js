5a1a48e345600fcc51f8d21b16a8d85d347ab224
belll218 <belinaz128@gmail.com>
2025-05-26 03:14:47 +0000

    created log.html and created table for user info in mysql

--- login.js
+++ login.js
@@ -3,7 +3,9 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: 'localhost',
   user: 'cocococonut',
-  password: 'password'
+  password: 'password',
+  databse: 'Bell_db',
+  port: 3306
 });
 
 con.connect(function(err) {
